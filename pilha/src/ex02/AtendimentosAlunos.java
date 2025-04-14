d77d101e40d95423e471be13040907e5fcecead6
FernandoNakasone <fernandonakasone040@gmail.com>
2025-04-14 10:40:04 -0300

    Ex 2 Atendimento Aluno concluido

--- pilha/src/ex02/AtendimentosAlunos.java
+++ pilha/src/ex02/AtendimentosAlunos.java
@@ -1,36 +1,56 @@
 package ex02;
 
+import ex01.FilaInt;
+
 import java.util.Scanner;
+
 
 public class AtendimentosAlunos {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        FilaInt fila = new FilaInt();
+        fila.init();
         int opcao;
 
-        do{
+        do {
             System.out.println("1 - Insere 1 aluno na fila");
             System.out.println("2 - Atende 1 aluno");
             System.out.println("3 - Encerra atendimento");
+            System.out.print("Opção:");
             opcao = sc.nextInt();
 
-            switch (opcao){
+            switch (opcao) {
                 case 1:
-
+                    System.out.print("RM do aluno:");
+                    int rm = sc.nextInt();
+                    fila.enqueue(rm);
                     break;
-
                 case 2:
-
+                    if (fila.isEmpty()) {
+                        System.out.println("Não há alunos aguardando na fila.");
+                    } else {
+                        System.out.println("Aluno a ser atendido agora :RM " + fila.dequeue());
+                    }
                     break;
 
                 case 3:
-                    System.out.println("Atendimento encerrado");
+                    if (fila.isEmpty()) {
+                        System.out.println("Atendimento encerrado");
+                    } else {
+                        System.out.println("Ainda há alunos a serem atendidos");
+                        System.out.println("Seleciona uma nova opção");
+                        opcao = 0;
+                    }
                     break;
 
                 default:
                     System.out.println("Opção inválida");
             }
-        }while (opcao != 3);
+
+            System.out.println("--------------------------------------------------------------------------------");
+
+        } while (opcao != 3);
 
         sc.close();
-;    }
+    }
 }
